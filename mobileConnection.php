9f5218a36171309bce52f9327f27a19f0d330b18
emilbunk <emilbunk@gmail.com>
2013-04-25 00:02:01 +0300

    Update mobileConnection.php

--- mobileConnection.php
+++ mobileConnection.php
@@ -3,16 +3,20 @@ include '/home/pi/UNICEF_VACCINE_MONITOR/E3131_smsGateway.php';
 
 $Response = getStatus();
 
-$connectionStatus = $Response -> ConnectionStatus;
-if($connectionStatus == 901) {
-	echo "Connected";
+if($Response){
+	$connectionStatus = $Response -> ConnectionStatus;
+	if($connectionStatus == 901) {
+		echo "Connected";
+	} else {
+		echo "Disconnected";
+	}
+	echo "Network type: ".$Respone -> CurrentNetworkType;
+	
+	echo "connection strength: ".$Response -> SignalStrength;
+	
+	echo "WanIPAddress: ".$Response -> WanIPAddress;
+	
+	echo "Primary DNS: ".$Respone -> PrimaryDns;
 } else {
-	echo "Disconnected";
+	echo "Could not connect to E3131";
 }
-echo "Network type: ".$Respone -> CurrentNetworkType;
-
-echo "connection strength: ".$Response -> SignalStrength;
-
-echo "WanIPAddress: ".$Response -> WanIPAddress;
-
-echo "Primary DNS: ".$Respone -> PrimaryDns;
