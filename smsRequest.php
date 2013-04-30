50f53c70b30a84c60b7f6cb595288c234f1fc687
emilbunk <emilbunk@gmail.com>
2013-04-30 15:15:39 +0200

    update

--- smsRequest.php
+++ smsRequest.php
@@ -52,11 +52,11 @@ if(!empty($Messages)){
 			break;
 			
 			case "sen": // Sensor
-				echo strtolower(substr($content, 7, 9));
-				switch (strtolower(substr($content, 7, 9))) {
+				echo strtolower(substr($content, 7, 3));
+				switch (strtolower(substr($content, 7, 3))) {
 					case "get":
 						echo "get";
-						$sensor = strtolower($content, 11, 24);
+						$sensor = strtolower($content, 11, 12);
 						echo $sensor;
 						$db->query("SELECT * FROM feeds WHERE name = '$sensor'");
 						if( $db == FALSE or $db->num_row < 1) {
@@ -69,7 +69,7 @@ if(!empty($Messages)){
 					
 					case "set":
 						echo "set";
-						$sensor = strtolower($content, 11, 24);
+						$sensor = strtolower($content, 11, 12);
 						echo $sensor; 
 						$db->query("SELECT * FROM feeds WHERE name = '$sensor'");
 						if( $db == FALSE or $db->num_row < 1) {
